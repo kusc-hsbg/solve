e5094beb91923ec1562414966271100e6619268f
junwon <formidable.hsbg@gmail.com>
2024-06-07 20:59:52 +0900

    upload

---
+++ coding test/Programmers/mysql/Select/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,22 @@
+SELECT 
+    r.REST_ID,
+    i.REST_NAME,
+    i.FOOD_TYPE,
+    i.FAVORITES,
+    i.ADDRESS,
+    ROUND(AVG(r.REVIEW_SCORE), 2) AS SCORE
+FROM 
+    REST_INFO i
+JOIN 
+    REST_REVIEW r ON i.REST_ID = r.REST_ID
+WHERE 
+    i.ADDRESS LIKE '서울%'
+GROUP BY 
+    r.REST_ID,
+    i.REST_NAME,
+    i.FOOD_TYPE,
+    i.FAVORITES,
+    i.ADDRESS
+ORDER BY 
+    SCORE DESC,
+    i.FAVORITES DESC;
